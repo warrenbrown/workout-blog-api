fa958d87644e2a65783e1b2ba3efde0e759a1954
warrenbrown <warren@katapultmedia.com>
2019-03-22 16:26:39 -0500

    Added routes and model for exercise

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+config = require('./config/databse');
 const api = require('./routes/api');
 
 const PORT = process.env.PORT || 3000
 const app = express();
+
+// Connect mongoose to our database
+mongoose.connect(config.uri, { useNewUrlParser: true }, (err) => {
+    if (err) {
+        console.log('Unable to connect to database', err)
+    } else {
+        console.log('Connected to database ' + config.db + ` at ${config.uri}`);
+    }
+});
+
+// Middleware 
+app.use(cors({ orgin: 'http//localhost:4200' }))
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.use("/api", api);
 app.listen(PORT);
 
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const Exercise = require('../models/exercise');
 
 router.get("/", (req, res) => {
   res.send("Welcome to the Workout app Api");
+});
+
+router.get('/exercises', (req, res) => {
+    Exercise.find({}, function(err, exercises) {
+        if(err) {
+            console.log('Error ,retrieving exercises', err)
+        } else {
+            res.json(exercises);
+        }
+    })
+})
+
+router.post('/exercises/new', (req, res) => {
+    console.log("Creating exercise.....");
+    Exercise.create(req.body, function(err, exercise) {
+        if(err) {
+            if(!req.body.title) {
+                console.log(err)
+                return res.json({ success: false, msg: 'Can not create exercise. Title is required' });
+            }
+
+            return res.json({ success: false, msg: 'Exercise not created.  Reps is required.'})
+        } else {
+            res.json({ success: true, msg: 'Exercise created.', exercise });
+        }
+    });
+});
+
+router.get('/exercises/:id', (req, res) => {
+    Exercise.findById(req.params.id, function(err, exercise) {
+        if(err) {
+            res.json({ success: false, msg: 'Invalid user name or id' })
+            console.log(err);
+        } else {
+            res.json({ success: true, msg: 'exercises found', exercise });
+        }
+    });
+});
+
+router.put('/exercises/edit/:id', (req, res) => {
+    Exercise.findByIdAndUpdate(req.params.id, req.body, function(err, exercise) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json({ success: true, msg: 'Exercise has been updated.', exercise });
+        }
+    });
+});
+
+router.delete('/exercises/delete/:id', (req, res) => {
+    Exercise.findByIdAndDelete(req.params.id, function(err, exercise) {
+        if(err) {
+            console.log(err)
+        }
+        res.json('Exercise has been deleted.')
+    });
 });
 
 module.exports = router;
